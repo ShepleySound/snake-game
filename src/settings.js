0f5d18909e7aa53ce50b914fe3b6fa039e6ebe7f
shepleysound <shepleysound@gmail.com>
2022-02-11 21:54:15 -0800

    Add background color changing.

--- src/settings.js
+++ src/settings.js
@@ -1,12 +1,15 @@
 class Settings {
   constructor(gameState) {
     this.gameState = gameState;
+    this.body = document.querySelector('body');
+    this.backgroundColor = this.getBackgroundColor();
     document
       .querySelector('#settings')
       .addEventListener('click', this.gameSettings.bind(this));
     document
       .querySelector('#back')
       .addEventListener('click', this.settingsBack.bind(this));
+    this.setBackgroundColor();
   }
 
   gameSettings() {
@@ -20,7 +23,14 @@ class Settings {
     document.querySelector('#state-overlay').style.display = 'flex';
   }
   getBackgroundColor() {
-    console.log(document.body);
+    const bodyStyles = window.getComputedStyle(this.body);
+    return bodyStyles.getPropertyValue('background-color');
+  }
+  setBackgroundColor() {
+    const colorPicker = document.querySelector('#backgroundcolor');
+    colorPicker.addEventListener('input', () => {
+      this.body.style.backgroundColor = colorPicker.value;
+    });
   }
 }
 
